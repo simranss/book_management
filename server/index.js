d2f9623129db5b90edb3938b482a466fd5b791f6
Simran Sharma <simran.s.sharma00@gmail.com>
2023-06-15 23:03:21 +0530

    Login and Registration API

--- server/index.js
+++ server/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session');
+const readerRouter = require('./routers/reader_router.js').readerRouter;
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/reader', session({secret: 'book_reader', resave: true, saveUninitialized: true}));
 
+app.use('/reader/book/*', function auth (req, res, next) {
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken']; // Access Token
+
+        jwt.verify(token, "access", (err, user)=>{
+            if (!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    } else {
+        return res.status(402).json({message: "User not logged in"})
+    }
+});
+
+const PORT = process.env.PORT;
+
+app.use('/reader', readerRouter);
+
+app.listen(PORT, () => console.log('Server is running on port', PORT));
---
+++ server/routers/reader_router.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let users = require('./../data/usersdb.js');
+
+const readerRouter = express.Router();
+
+const userExists = (email) => {
+    let validUsers = users.filter((user) => {
+        return (user.email === email);
+    });
+    if (validUsers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const authenticatedUser = (email, password) => {
+    let validUsers = users.filter((user) => {
+        return (user.email === email && user.password === password);
+    });
+    if (validUsers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+readerRouter.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!email || !password) {
+        return res.status(405).json({message: "email or password field empty"});
+    }
+
+    if(userExists(email)){
+        if (authenticatedUser(email, password)) {
+            let accessToken = jwt.sign({
+                data: password
+            }, 'access', { expiresIn: 60 * 60 });
+
+            req.session.authorization = {
+                accessToken, email
+            }
+            return res.status(200).send("User successfully logged in");
+        } else {
+            return res.status(401).json({message: "Incorrect password"});
+        }
+    } else {
+        return res.status(404).json({message: 'user does not exist'});
+    }
+});
+
+readerRouter.post("/register", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (email && password) {
+      if (!userExists(email)) {
+        users.push({"email": email, "password": password});
+        return res.status(200).json({message: "User successfully registered. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});
+      }
+    }
+    return res.status(405).json({message: "Email or password field empty."});
+});
+
+module.exports.readerRouter = readerRouter;
