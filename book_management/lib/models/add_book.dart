84cd98206b17a99e5e7f1354e83436368d9862ca
Simran Sharma <simran.s.sharma00@gmail.com>
2023-08-07 10:55:27 +0530

    Add book UI

--- book_management/lib/models/add_book.dart
+++ book_management/lib/models/add_book.dart
@@ -1,16 +1,38 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+
 class AddBookModel extends ChangeNotifier {
-  final formKey = GlobalKey<FormFieldState>();
+  final formKey = GlobalKey<FormState>();
 
   final titleController = TextEditingController();
   final descriptionController = TextEditingController();
   final pagesController = TextEditingController();
   final authorController = TextEditingController();
 
+  DateTime selectedDate = DateTime.now();
+
+  setSelectedDate(DateTime val) {
+    selectedDate = val;
+    notifyListeners();
+  }
+
   addBook() async {
-    if (formKey.currentState!.validate()) {
-      // add book
+    debugPrint('title: ${titleController.text.trim()}');
+    debugPrint('desc: ${descriptionController.text.trim()}');
+    debugPrint('author: ${authorController.text.trim()}');
+    debugPrint('pages: ${pagesController.text.trim()}');
+    debugPrint('release year: ${selectedDate.year}');
+    // add book
+
+    try {
+      var response = http.post(Uri.parse(''));
+    } on SocketException {
+      debugPrint('Internet Issues');
+    } catch (err) {
+      debugPrint('err: $err');
     }
   }
 }
